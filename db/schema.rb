4957a39dfd935cd0ee45b08bdf8a20a00a10bba4
taira120 <taira1999120@gmail.com>
2019-04-26 17:37:00 +0900

    Restaurantモデルを作った

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,10 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
